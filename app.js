c70ab5853b2d8a8ffa4565f01b9c3fe430e63bce
siddu <siddu13022004@gmail.com>
2024-02-12 19:48:17 +0530

    Image Being Displayed

--- app.js
+++ app.js
@@ -9,33 +9,32 @@ app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 });
 
+
 app.post("/",function(req,res){
   
   newsapi.articles({
-    source: 'associated-press', // required
-    sortBy: 'top' // optional
+    source: 'CNN' // required
+    // sortBy: 'top' // optional
   }).then(articlesResponse => {
-    // console.log(articlesResponse);
+    console.log(articlesResponse);
     let news=articlesResponse;
     // console.log(news.articles);
     res.set("Content-Type", "text/html");
 
     for(let i=0;i<news.articles.length;i++){
-      // console.log("Article No ",i+1);
-      // console.log("Author - ",news.articles[i].author);
-      // console.log("Title - ",news.articles[i].title);
-      // console.log("Url - ",news.articles[i].url);
-      // console.log("Image Url",news.articles[i].urlToImage);
-      // console.log("Description - ",news.articles[i].description);
+
+      
       let author=news.articles[i].author;
       let title=news.articles[i].title;
       let imgsrc=news.articles[i].urlToImage;
       let articlesrc=news.articles[i].url;
       let desc=news.articles[i].description;
-      res.write("<h3> Title </h3>- "+title);
-      res.write("<h4> Author </h4>- "+author);
-      res.write("<h3> Description </h3>- "+desc);
-      res.write("<br><a href='"+articlesrc+"' >Press to Main Link </a>");
+
+      res.write("<h3 class='title'> Title </h3>- "+title);
+      res.write("<h4 class='author'> Author </h4>- "+author);
+      res.write("<h3 class='desc'> Description </h3>- "+desc);
+      res.write("<br><a /href='"+articlesrc+"' >Press to Main Link </a>");
+      res.write("<img src='"+imgsrc+"' width=500px height=300px >");
       res.write("<hr>");
     }
 
